af62fd4f22de5bc79830186e37510060ebc5e332
Senthil <senthilhunt1512@gmail.com>
2025-02-12 20:54:25 +0530

    I frme Tested.

--- src/main/java/pageObjects/PracticePage.java
+++ src/main/java/pageObjects/PracticePage.java
@@ -4,12 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PracticePage {
     public WebDriver driver;
+    public WebDriverWait wait;
     public PracticePage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver,this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     @FindBy(xpath = "(//tr//th)[1]")
     WebElement header1;
@@ -26,6 +31,21 @@ public class PracticePage {
     @FindBy(xpath = "//h2[contains(text(),'Welcome to QAClick Academy ')]")
     WebElement aboutUs;
 
+    @FindBy(xpath = "//fieldset/iframe")
+    WebElement ifrmaeExample;
+
+    @FindBy(xpath = "//a[@href='/']")
+    WebElement homeMenu;
+
+    @FindBy(xpath = "//a[@class='dropdown-toggle']")
+    WebElement moreMenu;
+
+    @FindBy(xpath = "//a[@href='contact-us']")
+    WebElement contactUs;
+
+    @FindBy(xpath = "//h4[text()='Address']/following-sibling::br/following-sibling::text()")
+    WebElement address;
+
     public WebElement AboutUs(){
         return aboutUs;
     }
@@ -41,6 +61,21 @@ public class PracticePage {
     }
     public WebElement header3(){
         return header3;
+    }
+
+    //Method to switch to I frame
+    public void switchToIframe(){
+        driver.switchTo().frame(ifrmaeExample);
+    }
+
+    //Click on the home menu
+    public void clickOnHome(){
+        homeMenu.click();
+    }
+
+    //Hover to the more menu
+    public void hoverToMoreMenu(){
+
     }
 
 
